f998a5f5516fa9ec5c938dfe950e4f4b997bb0ee
oyevikki <79931609+oyevikki@users.noreply.github.com>
2023-02-03 19:06:07 +0530

    math set
    all button set

--- KidsLove/KidsLove/ViewController.swift
+++ KidsLove/KidsLove/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var showButtonTapped: UIButton!
+    
+    @IBAction func showButtonTappped(_ sender: Any) {
+        present(HomeViewController(), animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
 
 
---
+++ KidsLove/OperatorsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  OperatorsViewController.swift
+//  KidsLove
+//
+//  Created by Vikash on 03/02/23.
+//
+
+import UIKit
+
+class OperatorsViewController: UIViewController {
+
+    
+    @IBOutlet weak var plusButton: UIButton!
+    
+    
+    @IBOutlet weak var divideButton: UIButton!
+    @IBOutlet weak var productButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     plusButton.layer.cornerRadius = 0.5 * plusButton.bounds.size.width
+        divideButton.layer.cornerRadius = 0.5 * divideButton.bounds.size.width
+      productButton.layer.cornerRadius = 0.5 * productButton.bounds.size.width
+      minusButton.layer.cornerRadius = 0.5 * minusButton.bounds.size.width
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
