37cba35d9363cdee7b54cb1d5b133cd3daf2c219
Vishnu <vishnubishnoi7@gmail.com>
2023-02-04 13:45:10 +0530

    Question Screen
    Question Screen

--- KidsLove/KidsLove/ViewController.swift
+++ KidsLove/KidsLove/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func controllerbtn(_ sender: Any) {
+        present(QuestionViewController(), animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
